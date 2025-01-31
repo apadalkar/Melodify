2e13f670a98f5e650d5fa56944872a1d8192b428
Anit <apadalkar@berkeley.edu>
2025-01-31 01:25:40 -0800

    editing variables

--- melodify/next.config.mjs
+++ melodify/next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     env: {
       SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-      NEXT_PUBLIC_SPOTIFY_CLIENT_ID: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-      NEXT_PUBLIC_SPOTIFY_REDIRECT_URI: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
     },
     output: "standalone", 
     images: {
